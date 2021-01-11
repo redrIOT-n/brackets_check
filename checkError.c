e7f5db4a974d11eae47d0ef8af88068e470c34b5
redrIOT-n <75249528+redrIOT-n@users.noreply.github.com>
2021-01-11 11:09:04 -0800

    Update checkError.c

--- checkError.c
+++ checkError.c
@@ -29,7 +29,7 @@ int check(List *l, FILE *fh);
 int main(int argc, char *argv[]){
 
 	if(argc != 2){
-		fprintf(stderr, "Usage: ./checkbrackets.c <filename>\n");
+		fprintf(stderr, "Usage: ./checkError <filename>\n");
 		return 0;
 	}
 
